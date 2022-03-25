fa6ce80b1ac0b5702e3732f039b2813dcc3a49bd
Elkanah <elkanahawesome@gmail.com>
2022-03-25 15:36:22 -0500

    new ui

--- gameScript.js
+++ gameScript.js
@@ -1,27 +1,39 @@
 
-button = document.getElementById("rpsButton");
-button.addEventListener("click", play);
+buttons = document.querySelectorAll("button");
+buttons.forEach(
+    button => {this.addEventListener("click", play);
+});
+
 
 //stores players wins and bots
 let wins = 0;
 let robotWin = 0;
-let rpmHand = ["rock", "paper", "scissor"];
+let rpmHand = {0:"rock", 1:"paper", 2:"scissor", "rock":"🪨", "paper":"📑", "scissor":"✂️"};
+let rpmHandTwo = ["🪨", "📑", "✂️"];
 //function draws random number between 0-2
 function computerPlay() {
     console.log("inside computerPlay")
     return Math.floor(Math.random() * 3)
 
 }
+//display the hand on screen
+function displayHand(leftHand, rightHand) {
+    let left = document.querySelector(".left");
+    let right = document.querySelector(".right");
+    console.log(leftHand);
+    left.textContent = rpmHand[leftHand];
+    right.textContent = rpmHandTwo[rightHand];
+}
 
 //checks who won
 let checksWhoWon = function(playerSelection, computerMove) {
 
-    //cheap way to make userInput text not case sensitive by making them
-    //lowercase or uppercase
-    playerSelection.toLowerCase();
+    displayHand(playerSelection, computerMove);
+
     console.log("am i a global func or local func 🤔")
     //compares the random number the computer chose
     if (playerSelection == rpmHand[computerMove]) {
+        document.getElementById('winner').textContent = "Draw";
         console.log("draw");
         return "draw"
     }
@@ -29,37 +41,46 @@ let checksWhoWon = function(playerSelection, computerMove) {
     else if (playerSelection == "rock" && computerMove == 2) {
         console.log(`You win ${playerSelection} beats ${rpmHand[computerMove]}`)
         ++wins
-        return "You win rock  beats scissor";
+        whoWon = "You win rock 🤨 beats scissor";
+
+        document.getElementById('winner').textContent = whoWon
+
+        return whoWon;
     }
 
     else if (playerSelection == "paper" && computerMove == 0) {
         console.log(`You win ${playerSelection} beats ${rpmHand[computerMove]}`)
         ++wins
-        return "You win paper beats rock";
+        whoWon = "You win paper beats rock 🤨";
+
+        document.getElementById('winner').textContent = whoWon
+
+        return whoWon;
     }
 
     else if (playerSelection == "scissor" && computerMove == 1) {
         console.log(`You win ${playerSelection} beats ${rpmHand[computerMove]}`)
         ++wins
+        whoWon = "You win scissor beats paper";
+        document.getElementById('winner').textContent = whoWon
 
-        return  "You win scissor beats paper";
+        return whoWon;
     }
     //then returns the results
     ++robotWin
-    console.log("You lose L")
-    console.log(computerMove)
-    console.log(playerSelection)
-
-    return `You lose ${rpmHand[computerMove]} beats ${playerSelection}`;
+    
+    whoWon = `You lose ${rpmHand[computerMove]} beats ${playerSelection}`;
+    
+    document.getElementById('winner').textContent = whoWon
+    return whoWon;
+    
 };
 //picks winner
 function pickWinner(player1Wins, player2Wins) {
     //if player wins is the same as bots win then its a draw
-    if (player1Wins == player2Wins) {
-        console.log(robotWin);
-        console.log(wins);
-        console.log("Draw no one won");
-        return "Draw nobody won"
+    if (player1Wins < player2Wins) {
+        
+        return "Robot Wins"
     }
     //else if players wins is greater than bots player wins
     else if (player1Wins > player2Wins) {
@@ -74,45 +95,43 @@ function pickWinner(player1Wins, player2Wins) {
     return "You Lose TrollDespair";
 }
 
+//change score
+function updateScore(wins, robotWins) {
+    //select the score node
+    let humanScore = document.querySelector(".humanScore");
+    let robotScore = document.querySelector(".robotScore");
+
+    
+
+    //give elemenet new value
+    humanScore.textContent = wins;
+    robotScore.textContent = robotWins;
+
+}
+
 //function plays the game
-function play() {
-    //then loops through x amount times
-    // og val|test|increments
-    //       |case|  
-    for (x=0; x<5; x++) {
-        //if robot wins is equal to 4 or player then stop
-        if (wins == 4) {
-            console.log("YOU WON");
-            alert("You win PagMan");
-        }
+function play(userHand) {
+    let anounceWinner = document.querySelector(".actualWinner");
+    //clear winner text
+    anounceWinner.textContent = "";
+    let score = document.querySelector(".score");
 
-        else if (robotWin == 4) {
-            console.log("YOU LOST FailFish");
-            alert("You lost 🤦‍♂️");
-        }
-        //asks for player to input 👊 📄 ✂
-        let userInput = prompt("Choose between: Rock, paper, scissor.", "").toLowerCase();
-        console.log(typeof userInput)
-        //calls computerPlay and checksWhoWon
-        let winner = checksWhoWon(userInput, computerPlay());
-        alert(winner)
+    //figure out what button user pressed
+    let userPick = userHand.target.attributes[1].nodeValue;
+    
+    //calls computerPlay and checksWhoWon
+    let winner = checksWhoWon(userPick, computerPlay());
+
        
+    //update score 
+    updateScore(wins, robotWin);
 
-
-
+    //if robot wins is equal to 5 or player then stop
+    if (wins == 5 || robotWin == 5) {
+        
+        anounceWinner.innerText = pickWinner(wins, robotWin);
+        //alert("You win PagMan");
+        wins = 0;
+        robotWin = 0;
     }
-    //as the name suggests
-    alert(pickWinner(wins, robotWin));
-    //then resets
-    wins = 0;
-    robotWin = 0;
 }
-
-/*
-function reset() {
-    wins = 0;
-    robotWin = 0;
-}
-*/
-//function return string, string,. . ..........................................,
-//      number, string;
